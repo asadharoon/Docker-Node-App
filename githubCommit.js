b957b656d42139c13285ed1503918480b0c56a6f
Asad Haroon <asadharoon70@gmail.com>
2021-05-06 14:34:14 +0500

    childprocess test

--- githubCommit.js
+++ githubCommit.js
@@ -2,6 +2,14 @@ const { exec } = require("child_process");
 let args = process.argv;
 args.splice(0, 2);
 let str = args.join(" ");
-exec("git add .");
-exec(`git commit -m "${str}"`);
+exec("git add .", cb);
+exec(`git commit -m "${str}"`, cb);
 exec("git push origin main");
+
+function cb(err, strout, stdin) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+}
